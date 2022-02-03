22bfb4928150c19c6343bfdc5e1378d598abe832
Junwon Lee <cpprhtn@naver.com>
2022-02-03 19:24:24 +0900

    자바의 정석 3판

---
+++ src/com/cpprhtn/Hello.java
@@ -0,0 +1,8 @@
+package com.cpprhtn;
+
+public class Hello {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world.");
+    }
+}
---
+++ src/com/cpprhtn/Main.java
@@ -0,0 +1,8 @@
+package com.cpprhtn;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
