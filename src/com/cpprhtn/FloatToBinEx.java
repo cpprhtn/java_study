cefd6061ec00bbbb0b4b47fe56aeee7cf6cfc171
Junwon Lee <cpprhtn@naver.com>
2022-02-05 18:05:19 +0900

    실수형

---
+++ src/com/cpprhtn/FloatToBinEx.java
@@ -0,0 +1,11 @@
+package com.cpprhtn;
+
+public class FloatToBinEx {
+    public static void main(String args[]) {
+        float f = -9.1234567f;
+        int i = Float.floatToIntBits(f);
+
+        System.out.printf("%f%n", f);
+        System.out.printf("%X%n", i);
+    }
+}
