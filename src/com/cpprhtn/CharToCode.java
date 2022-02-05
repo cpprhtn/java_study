66c65869a1c554a577f76de47b71c4d1bb89adfe
Junwon Lee <cpprhtn@naver.com>
2022-02-05 14:39:36 +0900

    유니코드 변환

---
+++ src/com/cpprhtn/CharToCode.java
@@ -0,0 +1,13 @@
+package com.cpprhtn;
+
+public class CharToCode {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int code = (int)ch;
+
+        System.out.printf("%c=%d(%#X)%n",ch, code, code);
+
+        char hch = '가';
+        System.out.printf("%c=%d(%#X)%n", hch, (int)hch, (int)hch);
+    }
+}
