64a7c4c57a991d0c1597e83d5829af150fc17979
Junwon Lee <cpprhtn@naver.com>
2022-02-10 20:56:11 +0900

    Operator

---
+++ src/com/cpprhtn/chapter3/OperatorEx13.java
@@ -0,0 +1,12 @@
+package com.cpprhtn.chapter3;
+
+public class OperatorEx13 {
+    public static void main(String[] args) {
+        char c1 = 'a';
+
+        //char c2 =  c1+1;
+        char c2 = 'a'+1;
+
+        System.out.println(c2);
+    }
+}
