2cb2880a2b704afd16c149912247f0940f1f3ddd
Junwon Lee <cpprhtn@naver.com>
2022-02-14 19:09:01 +0900

    Operator

---
+++ src/com/cpprhtn/chapter3/OperatorEx19.java
@@ -0,0 +1,11 @@
+package com.cpprhtn.chapter3;
+
+public class OperatorEx19 {
+    public static void main(String args[]) {
+        int x = 10;
+        int y = 8;
+
+        System.out.printf("%d을 %d로 나누면, \n", x, y);
+        System.out.printf("몫 : %d, 나머지 :  %d입니다.\n", x / y, x % y);
+    }
+}
---
+++ src/com/cpprhtn/chapter3/OperatorEx20.java
@@ -0,0 +1,9 @@
+package com.cpprhtn.chapter3;
+
+public class OperatorEx20 {
+    public static void main(String[] args) {
+        System.out.println(-10%8);
+        System.out.println(10%-8);
+        System.out.println(-10%-8);
+    }
+}
