7df2028aaa2f3cb24966b01c6aa0b1e3ed204bb6
Junwon Lee <cpprhtn@naver.com>
2022-02-15 17:23:01 +0900

    Flow

---
+++ src/com/cpprhtn/chapter4/FlowEx23.java
@@ -0,0 +1,19 @@
+package com.cpprhtn.chapter4;
+
+public class FlowEx23 {
+    public static void main(String[] args) {
+        int i=11;
+
+        System.out.println("카운트다운.");
+
+        while(i--!=0) {
+            System.out.println(i);
+
+            for(int j=0;j<2_000_000_000;j++) {
+                ;
+            }
+        }
+
+        System.out.println("GAME OVER");
+    }
+}
---
+++ src/com/cpprhtn/chapter4/FlowEx8.java
@@ -0,0 +1,30 @@
+package com.cpprhtn.chapter4;
+
+import java.util.*;
+
+public class FlowEx8 {
+    public static void main(String[] args) {
+        char gender;
+        String regNo = "";
+
+        System.out.print("당신의 주민번호를 입력하세요.(000000-0000000)>");
+
+        Scanner scanner = new Scanner(System.in);
+        regNo = scanner.nextLine();
+
+        gender = regNo.charAt(7); // 입력받은 번호의 8번째 문자를 gender에 저장
+
+        switch(gender) {
+            case '1':
+            case '3':
+                System.out.println("당신은 남자입니다.");
+                break;
+            case '2':
+            case '4':
+                System.out.println("당신은 여자입니다.");
+                break;
+            default:
+                System.out.println("유효하지 않은 주민등록번호입니다.");;
+        }
+    }
+}
