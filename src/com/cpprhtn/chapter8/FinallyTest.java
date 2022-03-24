7c5324539190e3810ac3cb8a2aadefd015c90069
Junwon Lee <cpprhtn@naver.com>
2022-03-24 23:16:52 +0900

    Create FinallyTest.java

---
+++ src/com/cpprhtn/chapter8/FinallyTest.java
@@ -0,0 +1,24 @@
+package com.cpprhtn.chapter8;
+
+public class FinallyTest {
+    public static void main(String args[]) {
+        try {
+            startInstall();
+            copyFiles();
+            deleteTempFiles();
+        } catch (Exception e) {
+            e.printStackTrace();
+            deleteTempFiles();
+        }
+    }
+
+    static void startInstall() {
+        /* ... */
+    }
+    static void copyFiles() {
+        /* ... */
+    }
+    static void deleteTempFiles() {
+        /* ... */
+    }
+}
